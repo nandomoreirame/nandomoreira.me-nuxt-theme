13bd0ddef1895013587bd8e3285830b963d9a2c5
Fernando Moreira <nandomoreira.me@gmail.com>
2016-08-24 13:51:18 -0300

    Fix video tag

--- config.rb
+++ config.rb
@@ -103,9 +103,7 @@ helpers do
   end
 
   def youtube(video_id, video_width= 560, video_height=420)
-    video = "<div class="responsive-video">"
-    video + "<iframe width=\"#{video_width}\" height=\"#{video_height}\" src=\"//www.youtube.com/embed/#{video_id}?color=white&theme=light\"></iframe>"
-    video + "</div>"
+    video = "<div class=\"responsive-video\"><iframe width=\"#{video_width}\" height=\"#{video_height}\" src=\"//www.youtube.com/embed/#{video_id}?color=white&theme=light\"></iframe></div>"
   end
 end
 
