19235100035c561a53c580e0e4c61f7c0693ba5f
Fernando Moreira <nandomoreira.me@gmail.com>
2016-09-25 19:59:16 -0300

    Remove fixed header

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -10,22 +10,14 @@
       }, false);
     },
     update: function(scroll) {
-      this.textParallax.style['-webkit-transform'] = 'translateY('+ (scroll * .5) +'px)';
-      this.textParallax.style['-moz-transform'] = 'translateY('+ (scroll * .5) +'px)';
-      this.textParallax.style['transform'] = 'translateY('+ (scroll * .5) +'px)';
-      this.textParallax.style['opacity'] = (1 - ( scroll * .005 ));
+      if (scroll < 500) {
+        this.textParallax.style['-webkit-transform'] = 'translateY('+ (scroll * .5) +'px)';
+        this.textParallax.style['-moz-transform'] = 'translateY('+ (scroll * .5) +'px)';
+        this.textParallax.style['transform'] = 'translateY('+ (scroll * .5) +'px)';
+        this.textParallax.style['opacity'] = (1 - ( scroll * .005 ));
+      }
     }
   };
 
   titleParallax.init();
-
-  var header = document.querySelector('.header');
-
-  window.addEventListener("scroll", function(event) {
-    if (this.scrollY > 530) {
-      header.classList.add("is-fixed");
-    } else {
-      header.classList.remove("is-fixed");
-    }
-  }, false);
 })();
