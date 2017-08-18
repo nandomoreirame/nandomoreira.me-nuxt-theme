c01d1309462e66da381ebef28ffbdc24dd99283a
Fernando Moreira <nandomoreira.me@gmail.com>
2017-08-18 13:46:25 -0300

    Removed about translate en

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -3,20 +3,6 @@
 ###
 
 module CustomHelpers
-  def local_path(path, options={})
-    lang = options[:language] ? options[:language] : I18n.locale.to_s
-
-    if lang != "br"
-      "/#{lang}/#{path}"
-    else
-      "/#{path}"
-    end
-  end
-
-  def other_langs
-    langs - [I18n.locale]
-  end
-
   def youtube(video_id, video_width= 560, video_height=420)
     video = "<div class=\"responsive-video\"><iframe width=\"#{video_width}\" height=\"#{video_height}\" src=\"//www.youtube.com/embed/#{video_id}?color=white&theme=light\"></iframe></div>"
     return video
