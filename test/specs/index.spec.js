a4803077d14f39a667ea4f98d18c3b8117a895e6
Fernando Moreira <nandomoreira.me@gmail.com>
2017-10-01 00:49:50 -0300

    start unit tests

---
+++ test/specs/index.spec.js
@@ -0,0 +1,4 @@
+describe('List.vue', () => {
+  it('should', () => {
+  })
+})
---
+++ test/webpack.config.js
@@ -0,0 +1,11 @@
+var path = require('path')
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': path.join(__dirname, '../src')
+    }
+  }
+}
