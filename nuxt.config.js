49702afcdb2583d8532ab267b95929026365520d
Fernando Moreira <nandomoreira.me@gmail.com>
2017-10-01 01:41:37 -0300

    added humans.txt, meta tags and links for favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,18 +15,40 @@ module.exports = {
       { charset: 'utf-8' },
       { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Fernando Moreira | Front-end / UX Designer' },
-      { hid: 'keyword', name: 'keyword', content: 'front-end,front,wordpress,javascript,html,css,desenvolvedor,curitiba,paraná' }
+      { name: 'description', content: 'Fernando Moreira | Front-end / UX Designer' },
+      { name: 'keyword', content: 'front-end,front,wordpress,javascript,html,css,desenvolvedor,curitiba,paraná' },
+      { name: 'og:locale', content: 'pt_BR' },
+      { name: 'twitter:site', content: '@oseunando' },
+      { name: 'googlebot', content: 'index,follow' },
+      { name: 'robots', content: 'index,follow,noodp' },
+      { name: 'theme-color', content: '#0c59a0' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon/favicon-16x16.ico' },
+      { rel: 'apple-touch-icon', sizes: '57x57', href: '/favicon/apple-icon-57x57.png' },
+      { rel: 'apple-touch-icon', sizes: '60x60', href: '/favicon/apple-icon-60x60.png' },
+      { rel: 'apple-touch-icon', sizes: '72x72', href: '/favicon/apple-icon-72x72.png' },
+      { rel: 'apple-touch-icon', sizes: '76x76', href: '/favicon/apple-icon-76x76.png' },
+      { rel: 'apple-touch-icon', sizes: '114x114', href: '/favicon/apple-icon-114x114.png' },
+      { rel: 'apple-touch-icon', sizes: '120x120', href: '/favicon/apple-icon-120x120.png' },
+      { rel: 'apple-touch-icon', sizes: '144x144', href: '/favicon/apple-icon-144x144.png' },
+      { rel: 'apple-touch-icon', sizes: '152x152', href: '/favicon/apple-icon-152x152.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-icon-180x180.png' },
+      { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/favicon/android-icon-192x192.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon/favicon-96x96.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-192x192.png' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
+      { rel: 'manifest', href: '/favicon/manifest.json' },
+      { rel: 'author', href: '/humans.txt' },
+      { rel: 'me', href: 'nandomoreira.me@gmail.com' }
     ]
   },
   css: [
     { src: '~assets/sass/main.sass', lang: 'sass' }
   ],
   loading: {
-    color: '#3B8070',
+    color: '#0c59a0',
     height: '3px'
   },
   plugins: [
