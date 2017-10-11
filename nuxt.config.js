58eeecb34d1d69ee08f439f848de1622091109fc
Fernando Moreira <nandomoreira.me@gmail.com>
2017-10-10 21:43:07 -0300

    Update meta viewport and added more meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, minimum-scale=1.0, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes' },
       { name: 'og:locale', content: 'pt_BR' },
       { name: 'googlebot', content: 'index,follow' },
       { name: 'robots', content: 'index,follow,noodp' },
@@ -22,7 +22,10 @@ module.exports = {
       { name: 'twitter:card', content: 'summary_large_image' },
       { name: 'twitter:site', content: '@oseunando' },
       { name: 'country', content: 'Brazil' },
-      { name: 'revisit-after', content: '7 days' }
+      { name: 'revisit-after', content: '7 days' },
+      { name: 'mobile-web-app-capable', content: 'yes' },
+      { name: 'application-name', content: 'Fernando Moreira | Front-end / UX Designer' },
+      { name: 'generator', content: 'Visual Studio Code v1.17' }
     ],
     link: [
       { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon-16x16.ico' },
