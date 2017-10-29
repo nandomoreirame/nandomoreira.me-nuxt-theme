46543572825e3764f0aef0c8380f51f8a9830f90
Fernando Moreira <nandomoreira.me@gmail.com>
2017-10-28 23:17:52 -0200

    remove path bars

--- plugins/browser.js
+++ plugins/browser.js
@@ -7,47 +7,47 @@ const env = (process.env.NODE_ENV === 'production');
 
 const redirPathUrl = 'https://nandomoreirame.github.io'
 const arrPages = [
-  '/dotX',
-  '/lora',
-  '/lorem-gemidao-do-zap-ipsum',
-  '/end2end',
-  '/mix-buttons',
-  '/mug',
-  '/nandomoreira-jekyll-theme',
-  '/simplest',
-  '/zetsu',
-  '/middleman-blog',
-  '/chucknorris-jokes',
-  '/wololooooo',
-  '/simplegallery',
-  '/signature',
-  '/chef',
-  '/stormipsum',
-  '/bs4-grid',
-  '/comece-com-gulp',
-  '/gridfy',
-  '/hexo-blog',
-  '/jekyll-powerful',
-  '/bs4-alpha-login',
-  '/fmc-simple-modal',
-  '/Kanban-Projects',
-  '/menu-dropdown',
-  '/jogo-de-dados-canvas-html5',
-  '/canvas_menino',
-  '/bandeira-do-brasil',
-  '/simple.parallax',
-  '/uikit',
-  '/scrolltoo',
-  '/faisca-css',
-  '/projeto-final-web',
-  '/exemplos'
+  'dotX',
+  'lora',
+  'lorem-gemidao-do-zap-ipsum',
+  'end2end',
+  'mix-buttons',
+  'mug',
+  'nandomoreira-jekyll-theme',
+  'simplest',
+  'zetsu',
+  'middleman-blog',
+  'chucknorris-jokes',
+  'wololooooo',
+  'simplegallery',
+  'signature',
+  'chef',
+  'stormipsum',
+  'bs4-grid',
+  'comece-com-gulp',
+  'gridfy',
+  'hexo-blog',
+  'jekyll-powerful',
+  'bs4-alpha-login',
+  'fmc-simple-modal',
+  'Kanban-Projects',
+  'menu-dropdown',
+  'jogo-de-dados-canvas-html5',
+  'canvas_menino',
+  'bandeira-do-brasil',
+  'simple.parallax',
+  'uikit',
+  'scrolltoo',
+  'faisca-css',
+  'projeto-final-web',
+  'exemplos'
 ];
 
 export default ({ app }) => {
   /*
   ** Only run on client-side and only in production mode
   */
-  if (!env) return;
+  // if (!env) return;
 
   /*
   ** Set the current page
@@ -64,7 +64,7 @@ export default ({ app }) => {
     ga('set', 'page', to.fullPath);
     ga('send', 'pageview');
 
-    if (arrPages.includes(to.path)) {
+    if (arrPages.includes(to.path.replace(/\//g,''))) {
       window.location = `${redirPathUrl}${to.path}`;
     }
   });
