7f5605e1733671f2302338af2b3764ebecacf9c3
Fernando Moreira <nandomoreira.me@gmail.com>
2017-11-14 17:18:58 -0200

    Remove Google tag google-site-verification and add html file

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,6 @@ module.exports = {
       { charset: 'utf-8' },
       { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
       { name: 'viewport', content: 'width=device-width, minimum-scale=1.0, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes' },
-      { name: 'google-site-verification', content: 'zS1dSn20XtA4FJYEOQLXqI0boxZdMnJ2g3beje-cl20' },
       { name: 'og:locale', content: 'pt_BR' },
       { name: 'googlebot', content: 'index,follow' },
       { name: 'robots', content: 'index,follow,noodp' },
