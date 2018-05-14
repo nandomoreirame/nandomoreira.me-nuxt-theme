e3c05efdb6a8c88a0770956753e4e936a799b366
Fernando Moreira <nandomoreira.me@gmail.com>
2018-05-14 19:22:02 -0300

    Remove count comments

--- components/PostItem.vue
+++ components/PostItem.vue
@@ -20,10 +20,6 @@
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
               por <strong itemprop="author">Fernando Moreira</strong>
             </span>
-            <span>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-circle"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"/></svg>
-              <strong class="disqus-comment-count" :data-disqus-identifier="splitIdentifier(post.permalink)">Carregando comentários...</strong>
-            </span>
           </small>
         </footer>
       </div>
